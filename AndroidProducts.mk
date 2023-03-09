067482eb618a6ab5e64e3a63dac659eea9c6ce34
klozz <carlosj@klozz.dev>
2023-03-09 03:36:05 +0000

    xperience bringup
    Signed-off-by: klozz <carlosj@klozz.dev>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_alioth.mk
+    $(LOCAL_DIR)/xperience_alioth.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_alioth-user \
-    aosp_alioth-userdebug \
-    aosp_alioth-eng
+    xperience_alioth-user \
+    xperience_alioth-userdebug \
+    xperience_alioth-eng

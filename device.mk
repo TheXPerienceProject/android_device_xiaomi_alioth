7d69460513249200dff4033745deca09a9566fc3
EmanuelCN <emanuelghub@gmail.com>
2023-05-29 13:16:40 +0000

    Revert "alioth: Add shim for missing symbol in com.mi.node.watermark.so"
    This reverts commit 24d82f2fadb843f725354e1398d697630baa0738.

--- device.mk
+++ device.mk
@@ -37,10 +37,6 @@ PRODUCT_PACKAGES += \
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 30
 
-# Shims
-PRODUCT_PACKAGES += \
-    lib-watermarkshim
-
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
